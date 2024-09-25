d5b84b970a5cd20e3c9d829395bd31a45b990e6b
Maks <makzylinski@gmail.com>
2024-09-25 13:11:44 +0200

    Grid

--- src/app/components/game-view/game-view.component.ts
+++ src/app/components/game-view/game-view.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RangePipe } from '../../pipes/range.pipe';
 
 @Component({
   selector: 'trf-game-view',
   standalone: true,
-  imports: [],
+  imports: [RangePipe],
   templateUrl: './game-view.component.html',
-  styleUrl: './game-view.component.scss'
+  styleUrl: './game-view.component.scss',
 })
 export class GameViewComponent {
+  items = [1, 2];
+  scrHeight: number = 0;
+  scrWidth: number = 0;
+  gridWidth: number = 0;
+  gridHeight: number = 0;
 
+  @HostListener('window:resize', ['$event'])
+  getScreenSize() {
+    this.scrHeight = window.innerHeight;
+    this.scrWidth = window.innerWidth;
+    console.log(this.scrHeight, this.scrWidth);
+    console.log(typeof this.scrHeight);
+    this.calculateGrid();
+  }
+
+  constructor() {
+    this.getScreenSize();
+  }
+
+  calculateGrid() {
+    this.gridWidth = Math.round(Math.floor(this.scrWidth / 19));
+    this.gridHeight = Math.round(Math.floor(this.scrHeight / 19));
+  }
 }
